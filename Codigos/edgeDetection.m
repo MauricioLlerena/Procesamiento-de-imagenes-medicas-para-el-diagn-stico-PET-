acd9847d59db7f98246c819eeb14ccebc8328152
a01610820 <105745071+a01610820@users.noreply.github.com>
2022-05-17 15:18:32 -0500

    Ya estuvo

--- Codigos/edgeDetection.m
+++ Codigos/edgeDetection.m
@@ -22,12 +22,14 @@ imshow(g2,[-10,10])
 figure(2)
 subplot(1,2,1)
 imshow(g1,[-10,10])
+title("Dx")
 subplot(1,2,2)
 imshow(g2,[-10,10])
-
+title("Dy")
 %%
 figure(3)
 subplot(1,1,1)
+%%
 %Mascara de sobel dx y dy para gradiente
 edgex=[1,0,-1;2,0,-2;1,0,-1]/8
 gx=conv2(f,edgex,'same');
@@ -35,7 +37,8 @@ gy=conv2(f,edgey,'same');
 %Gradiente de magnitud
 mag=abs(gx)+abs(gy);
 imshow(mag,[]);
-
+title("Gradient Magnitude |dx|+|gy|")
+%%
 noisemask = [-1, 0 1];
 noiseimage = conv2(f,noisemask,'same');
 noisevariance = mean2(noiseimage.^2);
@@ -44,12 +47,15 @@ edgedetection1 = mag > noisestd;
 edgedetection2 = mag > 2*noisestd;
 subplot(1,2,1)
 imshow(edgedetection1,[]);
+title("Edge detection at sigma")
 subplot(1,2,2)
 imshow(edgedetection2,[]);
+title("Edge detection 2 at sigma")
 figure(4)
 subplot(1,1,1)
 angle=atan2(gy,gx);
 imshow(angle,[]);
-
+title("Gradient orientation")
+%%
 edgcany=edge(f,'Canny');
 imshow(edgcany,[]);
