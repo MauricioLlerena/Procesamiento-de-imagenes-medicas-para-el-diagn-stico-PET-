a7f4a0ee842f3ebffe1dd6a763220e4b5bbd12cd
Jesús Gabriel Leos Perales <A01412140@tec.mx>
2022-05-19 20:29:07 -0500

    Doppler Response 19 de Mayo Tamez

--- Codigos/Doppler_Response_3.m
+++ Codigos/Doppler_Response_3.m
@@ -52,3 +52,83 @@ hrdresp = phased.RangeDopplerResponse('RangeMethod', 'FFT',...
 figure(2)
 plotResponse(hrdresp, RangeDopplerEx_Dechirp_X,...
     RangeDopplerEx_Dechirp_Xref, 'Unit', 'db', 'NormalizeDoppler', true)
+
+%% Range speed response pattern of target
+
+%4.1 Set initial specifications
+
+antenna = phased.IsotropicAntennaElement(...
+    'FrequencyRange', [5e9 15e9]);
+transmitter = phased.Transmitter('Gain', 20, 'InUseOutputPort', true);
+fc = 10e9;
+target = phased.RadarTarget('Model', 'Nonfluctuating', ...
+    'MeanRCS', 1, 'OperatingFrequency', fc);
+txloc = [0,0,0]; %location for the antenna, global origina, introduces as a vector[x, y, z]
+tgtloc = [5000,5000,10]; %location for the target, introduce as a vector [x, y, z]
+antennaplatform = phased.Platform('InitialPosition', txloc);
+targetplatform = phased.Platform('InitialPosition', tgtloc);
+[tgtrng, tgtang] = rangeangle(targetplatform.InitialPosition, ...
+    antennaplatform.InitialPosition);
+
+%4.2 Plot the range-Doppler response
+
+waveform = phased.RectangularWaveform('PulseWidth', 2e-6,'OutputFormat', 'Pulses', 'PRF', 1e4, 'NumPulses', 1);
+c = physconst('Lightspeed');
+maxrange = c/(2*waveform.PRF);
+SNR = npwgnthresh(1e-6, 1, 'noncoherent');
+lambda = c/target.OperatingFrequency;
+maxrange = c/(2*waveform.PRF);
+tau = waveform.PulseWidth;
+Ts = 290;
+dbterm = db2pow(SNR - 2*transmitter.Gain);
+Pt = (4*pi)^3*physconst('Boltzmann')*Ts/tau/target.MeanRCS/lambda^2*maxrange^4*dbterm;
+
+%4.3 Set the peak transmit power to the value obtained from the radar
+%equation
+
+transmitter.PeakPower = ;
+
+%4.4 Create radiator and collector objects that operate at 10 GHz. Create a
+%free space path for the propagation of the pulse to and from the target.
+%Then, create a receiver.
+
+radiator = phased.Radiator(...
+    'PropagationSpeed', c, ...
+    'OperatingFrequency', fc, 'Sensor', antenna);
+channel = phased.FreeSpace(...
+    'PropagationSpeed', c, ...
+    'OperatingFrequency', fc, 'TwoWayPropagation', false);
+collector = phased.Collector(...
+    'PropagationSpeed', c, ...
+    'OperatingFrequency', fc, 'Sensor', antenna);
+receiver = phased.ReceiverPreamp('NoiseFigure', 0, ...
+    'EnableInputPort', true, 'SeedSource', 'Property', 'Seed', 2e3);
+
+%4.5 Propagate 25 pulses to and from the target. Collect the echoes at the
+%receiver, and store them in a column matrix named rx_puls.
+
+numPulses = 25;
+rx_puls = zeros(100, numPulses);
+
+for n = 1:numPulses
+    wf = waveform();
+    [wf, txstatus] = transmmitter(wf);
+    wf = radiator(wf, tgtang);
+    wf = channel(wf, txloc, tgtloc, [0;0;0], [0;0;0]);
+    wf = target(wf);
+    wf = channel(wf, tgtloc, txloc, [0;0;0], [0;0;0]);
+    wf = collector(wf, tgtang);
+    rx_puls(:,n) = receiver(wf, ~txstatus);
+end
+
+%4.6 Create a range-Doppler response object tat uses the matched filter
+%approach. Configure this object to show radial speed rather than Doppler
+%frequency. Use plotResponse to plot the range versus speed.
+
+rangedoppler = phased.RangeDopplerResponse(...
+    'RangeMethod', 'Matched filter', ...
+    'PropagationSpeed', c, ...
+    'DopplerOutput', 'Speed', 'OperatingFrequency', fc);
+
+figure(3)
+plotResponse(rangedoppler, rx_puls, getMatchedFilter(waveform));
